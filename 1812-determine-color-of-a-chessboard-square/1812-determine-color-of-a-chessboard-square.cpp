08d87fa74cffcbab3063ad65594103e76f98948f
patel vedant <vedantpatelxy12@gmail.com>
2026-07-28 13:46:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.05%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
-       return ((int)coordinates[0] +  coordinates[1] ) %2==1;
+       return (coordinates[0] +  coordinates[1] ) %2==1;
      }
 };
